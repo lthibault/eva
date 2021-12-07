56bc31b023ed141d8f44eec6b10e264279bd1732
Louis Thibault <l.thibault@sentimens.com>
2021-12-07 12:23:20 -0500

    Implement string addition and multiplication.

--- value.go
+++ value.go
@@ -1,6 +1,10 @@
 package eva
 
-import "unsafe"
+import (
+	"fmt"
+	"strings"
+	"unsafe"
+)
 
 const (
 	Int32 = iota
@@ -8,6 +12,17 @@ const (
 )
 
 type Type uint8
+
+func (t Type) String() string {
+	switch t {
+	case Int32:
+		return "int32"
+	case String:
+		return "string"
+	default:
+		panic(fmt.Sprintf("invalid type '%0x'", uint8(t)))
+	}
+}
 
 type Value struct {
 	// The first two fields have a memory representation equivalent
@@ -30,7 +45,7 @@ func NewInt32(i int32) Value {
 
 func NewString(s string) Value {
 	v := Value{Type: String}
-	v = *(*Value)(unsafe.Pointer(&s))
+	*(*string)(unsafe.Pointer(&v)) = s
 	return v
 }
 
@@ -38,3 +53,44 @@ func (v Value) IsPointer() bool { return uintptr(v.ptr) != 0 }
 
 func (v Value) Int32() int32   { return int32(v.val) }
 func (v Value) String() string { return *(*string)(unsafe.Pointer(&v)) }
+
+func Add(v0, v1 Value) (Value, error) {
+	// same type ?
+	if v0.Type^v1.Type == 0 {
+		switch v0.Type {
+		case String:
+			return NewString(v0.String() + v1.String()), nil
+
+		case Int32:
+			return NewInt32(v0.Int32() + v1.Int32()), nil
+		}
+	}
+
+	return Value{}, fmt.Errorf("cannot add type '%s' to '%s'", v0.Type, v1.Type)
+}
+
+func Mul(v0, v1 Value) (Value, error) {
+	if v1.Type == Int32 {
+		switch v0.Type {
+		case Int32:
+			return NewInt32(v0.Int32() * v1.Int32()), nil
+
+		case String:
+			var (
+				b strings.Builder
+				s = v0.String()
+				i = v1.Int32()
+			)
+
+			if i > 0 {
+				for b.Grow(int(i)); i > 0; i-- {
+					b.WriteString(s)
+				}
+			}
+
+			return NewString(b.String()), nil
+		}
+	}
+
+	return Value{}, fmt.Errorf("cannot multiply type '%s' with '%s'", v0.Type, v1.Type)
+}
--- value_test.go
+++ value_test.go
@@ -3,10 +3,12 @@ package eva_test
 import (
 	"fmt"
 	"math"
+	"strings"
 	"testing"
 
 	"github.com/lthibault/eva"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestValue_IsPointer(t *testing.T) {
@@ -34,55 +36,219 @@ func BenchmarkValue_IsPointer(b *testing.B) {
 
 func TestValue_Int32(t *testing.T) {
 	t.Parallel()
+	t.Helper()
 
-	for _, tt := range []struct {
-		message string
-		i       int32
-	}{{
-		message: "zero should be valid",
-	}, {
-		message: "max int32 should be valid",
-		i:       math.MaxInt32,
-	}, {
-		message: "min int32 should be valid",
-		i:       math.MinInt32,
-	}} {
-		assert.Equal(t, tt.i, eva.NewInt32(tt.i).Int32(), tt.message)
-	}
+	t.Run("Alloc", func(t *testing.T) {
+		t.Parallel()
+
+		for _, tt := range []struct {
+			message string
+			i       int32
+		}{{
+			message: "zero should be valid",
+		}, {
+			message: "max int32 should be valid",
+			i:       math.MaxInt32,
+		}, {
+			message: "min int32 should be valid",
+			i:       math.MinInt32,
+		}} {
+			assert.Equal(t, tt.i, eva.NewInt32(tt.i).Int32(), tt.message)
+		}
+	})
+
+	t.Run("Add", func(t *testing.T) {
+		t.Parallel()
+
+		for _, tt := range []struct {
+			message    string
+			arg0, arg1 eva.Value
+			want       int32
+			err        string
+		}{{
+			message: "mismatched types should fail",
+			arg0:    eva.NewInt32(42),
+			arg1:    eva.NewString("fail"),
+		}, {
+			message: "should successfully add two int32 types",
+			arg0:    eva.NewInt32(42),
+			arg1:    eva.NewInt32(3),
+			want:    45,
+		}} {
+			got, err := eva.Add(tt.arg0, tt.arg1)
+			if tt.err != "" {
+				assert.EqualError(t, err, tt.err)
+			} else {
+				assert.Equal(t, tt.want, got.Int32())
+			}
+		}
+	})
+
+	t.Run("Mul", func(t *testing.T) {
+		t.Parallel()
+
+		for _, tt := range []struct {
+			message    string
+			arg0, arg1 eva.Value
+			want       int32
+			wantErr    bool
+		}{{
+			message: "mismatched types should fail",
+			arg0:    eva.NewInt32(42),
+			arg1:    eva.NewString("fail"),
+			wantErr: true,
+		}, {
+			message: "should successfully multiply two int32 types",
+			arg0:    eva.NewInt32(21),
+			arg1:    eva.NewInt32(2),
+			want:    42,
+		}} {
+			got, err := eva.Mul(tt.arg0, tt.arg1)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				if assert.NoError(t, err) {
+					assert.Equal(t, tt.want, got.Int32())
+				}
+			}
+		}
+	})
 }
 
 func BenchmarkValue_Int32(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		if eva.NewInt32(int32(i)).Int32() != int32(i) {
-			panic(fmt.Sprintf("expected %d, got %d", i, eva.NewInt32(int32(i)).Int32()))
+	b.Helper()
+
+	b.Run("Alloc", func(b *testing.B) {
+		for i := 0; i < b.N; i++ {
+			if eva.NewInt32(int32(i)).Int32() != int32(i) {
+				panic(fmt.Sprintf("expected %d, got %d", i, eva.NewInt32(int32(i)).Int32()))
+			}
 		}
-	}
+	})
+
+	b.Run("Add", func(b *testing.B) {
+		var (
+			arg0 = eva.NewInt32(42)
+			arg1 = eva.NewInt32(3)
+		)
+
+		b.ResetTimer()
+		if _, err := eva.Add(arg0, arg1); err != nil {
+			panic(err)
+		}
+	})
+
+	b.Run("Mul", func(b *testing.B) {
+		var (
+			arg0 = eva.NewInt32(42)
+			arg1 = eva.NewInt32(3)
+		)
+
+		b.ResetTimer()
+		if _, err := eva.Mul(arg0, arg1); err != nil {
+			panic(err)
+		}
+	})
 }
 
 func TestValue_String(t *testing.T) {
 	t.Parallel()
+	t.Helper()
 
-	for _, tt := range []struct {
-		message string
-		s       string
-	}{{
-		message: "empty string should be valid",
-	}, {
-		message: "non-empty string should be valid",
-		s:       "hello, eva!",
-	}} {
-		assert.Equal(t, tt.s, eva.NewString(tt.s).String(), tt.message)
-	}
+	t.Run("Alloc", func(t *testing.T) {
+		t.Parallel()
+
+		for _, tt := range []struct {
+			message string
+			s       string
+		}{{
+			message: "empty string should be valid",
+		}, {
+			message: "non-empty string should be valid",
+			s:       "hello, eva!",
+		}} {
+			assert.Equal(t, tt.s, eva.NewString(tt.s).String(), tt.message)
+		}
+	})
+
+	t.Run("Add", func(t *testing.T) {
+		t.Parallel()
+
+		v, err := eva.Add(eva.NewString("foo"), eva.NewString("bar"))
+		require.NoError(t, err)
+		assert.Equal(t, "foobar", v.String())
+	})
+
+	t.Run("Mul", func(t *testing.T) {
+		t.Parallel()
+
+		var b strings.Builder
+		for i := 0; i < 100; i++ {
+			b.WriteString("foobar")
+		}
+
+		for _, tt := range []struct {
+			message    string
+			arg0, arg1 eva.Value
+			want       string
+			wantErr    bool
+		}{{
+			message: "should fail if first argument is not of type 'string'",
+			arg0:    eva.NewInt32(42),
+			arg1:    eva.NewString("fail"),
+			wantErr: true,
+		}, {
+			message: "should successfully multiply string by int32",
+			arg0:    eva.NewString("foobar"),
+			arg1:    eva.NewInt32(100),
+			want:    b.String(),
+		}} {
+			got, err := eva.Mul(tt.arg0, tt.arg1)
+			if tt.wantErr {
+				assert.Error(t, err)
+			} else {
+				assert.Equal(t, tt.want, got.String())
+			}
+		}
+	})
 }
 
 func BenchmarkValue_String(b *testing.B) {
-	var (
-		s  = "hello, eva!"
-		es = eva.NewString(s)
-	)
+	b.Helper()
 
-	b.ResetTimer()
-	for i := 0; i < b.N; i++ {
-		_ = es.String()
-	}
+	b.Run("Alloc", func(b *testing.B) {
+		var (
+			s  = "hello, eva!"
+			es = eva.NewString(s)
+		)
+
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			_ = es.String()
+		}
+	})
+
+	b.Run("Add", func(b *testing.B) {
+		var (
+			arg0 = eva.NewString("foo")
+			arg1 = eva.NewString("bar")
+		)
+
+		b.ResetTimer()
+		if _, err := eva.Add(arg0, arg1); err != nil {
+			panic(err)
+		}
+	})
+
+	b.Run("Mul", func(b *testing.B) {
+		var (
+			arg0 = eva.NewString("foo")
+			arg1 = eva.NewInt32(100)
+		)
+
+		b.ResetTimer()
+		if _, err := eva.Mul(arg0, arg1); err != nil {
+			panic(err)
+		}
+	})
 }
