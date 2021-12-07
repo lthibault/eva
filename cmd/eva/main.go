092f79a85c1d6f2028760306d539b8e0cee9b9b6
Louis Thibault <l.thibault@sentimens.com>
2021-12-07 13:05:43 -0500

    Stub call to parser.

--- cmd/eva/main.go
+++ cmd/eva/main.go
@@ -3,17 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/lthibault/eva"
 )
 
+const program = `
+	(+ "Hello, " "Eva!")
+`
+
 func main() {
 	var vm eva.VM
-
-	v, err := vm.Exec(`
-		42
-	`)
-
+	v, err := vm.Exec(strings.NewReader(program))
 	if err != nil {
 		log.Fatal(err)
 	}
